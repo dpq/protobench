59e479352d10c79f4d589bfbd728d9deae99abf6
David Parunakian <jaffar.rumith@gmail.com>
2012-05-27 18:43:38 +0400

    Moved to Amazon AWS hosting

--- servers.sh
+++ servers.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-SERVERS="83.69.226.156 83.69.226.241 83.69.226.251 83.69.226.252"
+PROXY="ec2-176-34-203-194.eu-west-1.compute.amazonaws.com"
+SERVERS="ec2-54-247-146-167.eu-west-1.compute.amazonaws.com ec2-54-247-150-55.eu-west-1.compute.amazonaws.com ec2-176-34-169-54.eu-west-1.compute.amazonaws.com ec2-54-247-154-201.eu-west-1.compute.amazonaws.com"
