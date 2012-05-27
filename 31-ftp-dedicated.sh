e6b53aea35e784e861315c3365366b267cd9b766
David Parunakian <jaffar.rumith@gmail.com>
2012-05-27 20:11:06 +0400

    Fixed the ftp/http public vs dedicated link comparison

--- 31-ftp-dedicated.sh
+++ 31-ftp-dedicated.sh
@@ -7,8 +7,8 @@ for i in `seq $REPLICATION`; do
   START=`date +%s.%N`
   wget -q ftp://anonymous@192.168.100.1/1024.bin
   END=`date +%s.%N`
-  eval R$i=`echo $END - $START | bc | sed "s/.\{6\}$//"`
-  rm 1024.bin;
+  eval R$i=`echo 8*1024/\($END - $START\) | bc | sed "s/.\{6\}$//"`
+  rm 1024.bin
 done
 
 stats
--- 42-http-public.sh
+++ 42-http-public.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 
-RUNS=25
+. stats.sh
+. config.sh
 
-for i in `seq $RUNS`; do
+for i in `seq $REPLICATION`; do
   START=`date +%s.%N`
   wget -q http://213.131.1.8/1024.bin
   END=`date +%s.%N`
-  eval R$i=`echo $END - $START | bc | sed "s/.\{6\}$//"`
-  rm 1024.bin;
+  eval R$i=`echo 8*1024/\($END - $START\) | bc | sed "s/.\{6\}$//"`
+  rm 1024.bin
 done
 
-
-RS=0
-for i in `seq $RUNS`; do
-  RS=$(eval echo \$R$i + $RS | bc)
-done
-
-#echo $(echo "scale=2; $RUNS*1073.741824*8 / $RS" | bc)
-SIGMA=0
-for i in `seq $RUNS`; do
-  delta=$(eval echo \$R$i - $RS)
-  SIGMA=`echo "scale=2; $SIGMA + $delta^2"`
-done
-
-echo Mean $(echo "scale=2; $RUNS*1073.741824*8 / $RS " | bc)
-echo Sigma $(echo "scale=2; sqrt($SIGMA/$RUNS) " | bc)
-
+stats
