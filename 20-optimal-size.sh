558bc150a084384b069e67174604ce8cc0b864ae
David Parunakian <jaffar.rumith@gmail.com>
2012-05-27 18:45:56 +0400

    Removed commented out code. Usings the stats function now

--- 20-optimal-size.sh
+++ 20-optimal-size.sh
@@ -6,19 +6,7 @@
 for SIZE in $SIZES; do
   for i in `seq $REPLICATION`; do
     eval R$i=`./mktorrent.sh $SIZE.bin`
-    #echo $TIME
-    #if [[ ${TIME:0:1} == '.' ]]; then
-    #  TIME=0$TIME
-    #fi
-    #echo $TIME
-    #eval R$i=$TIME
-    #TIME=`echo $TIME + $( ./mktorrent.sh $SIZE.bin ) | bc`
   done
-  #TIME=`echo "scale=3; $TIME / $REPLICATION" | bc`
-  #if [[ ${TIME:0:1} == '.' ]]
-  #then
-  # TIME=0$TIME
-  #fi
   echo $SIZE Mb
   stats
 done
