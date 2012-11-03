39a331b4b9dfd2011435889db613697858a08823
David Parunakian <dp@glavbot.ru>
2012-11-03 17:48:06 +0400

    Bugfixes; haproxy config file generation

--- 11-init.sh
+++ 11-init.sh
@@ -4,30 +4,44 @@
 . clients.sh
 . config.sh
 
+cp haproxy.cfg haproxy.cfg.tmp
+IFS=' ' read -ra HOSTS <<< "$SERVERS"
+a=1
+for i in "${HOSTS[@]}"; do
+	cat haproxy.cfg.tmp | sed s/_SERV_$a/$i/ > haproxy.cfg.mod
+    mv haproxy.cfg.mod haproxy.cfg.tmp
+    a=$(($a+1))
+done
+exit
+
 sudo apt-get update && sudo apt-get -y install bittorrent openssh-client wget build-essential
 
 wget http://ignum.dl.sourceforge.net/project/udt/udt/4.10/udt.sdk.4.10.tar.gz
 a=1
 IFS=' ' read -ra HOSTS <<< "$SERVERS"
 for i in "${HOSTS[@]}"; do
-  ssh p$i mkfs.ext3 /dev/xvdf1
-  ssh p$i mkdir /home/protobench
-  ssh p$i echo "/dev/xvdf1 /home/protobench ext3 defaults 0 0" >> /etc/fstab
-  ssh p$i mount /home/protobench
-  ssh p$i apt-get update
-  ssh p$i apt-get -y install nginx vsftpd wget bittorrent globus-gridftp-server-progs libglobus-gridftp-server-dev
-  ssh p$i wget $DATAHOST/1024.bin.torrent -O /home/protobench/1024.bin.torrent
-  ssh p$i wget -bq $DATAHOST/1024.bin -o log -O /home/protobench/1024.bin
+  ssh p$a mkfs.ext3 /dev/xvdf1
+  ssh p$a mkdir /home/protobench
+  ssh p$a echo "/dev/xvdf1 /home/protobench ext3 defaults 0 0" >> /etc/fstab
+  ssh p$a mount /home/protobench
+  ssh p$a apt-get update
+  ssh p$a apt-get -y install nginx vsftpd wget bittorrent globus-gridftp-server-progs libglobus-gridftp-server-dev
+  ssh p$a wget $DATAHOST/1024.bin.torrent -O /home/protobench/1024.bin.torrent
+  ssh p$a wget -bq $DATAHOST/1024.bin -o log -O /home/protobench/1024.bin
   scp udt.sdk.4.10.tar.gz p$i:/home/protobench
-  ssh p$i 'cd /home/protobench && tar -xzf udt.sdk.4.10.tar.gz && cd udt4/src && make && cd ../app && make'
-  ssh p$i 'wget http://www.globus.org/ftppub/gt5/5.2/5.2.0/installers/repo/globus-repository-squeeze_0.0.2_all.deb && dpkg -i globus-repository-squeeze_0.0.2_all.deb && aptitude update && tasksel install globus-gridftp && aptitude -y upgrade && aptitude -y install globus-simple-ca'
-  ssh p$i ln -s /home/protobench/ /var/www
-  ssh p$i invoke-rc.d nginx start
+  ssh p$a 'cd /home/protobench && tar -xzf udt.sdk.4.10.tar.gz && cd udt4/src && make && cd ../app && make'
+  ssh p$a 'wget http://www.globus.org/ftppub/gt5/5.2/5.2.0/installers/repo/globus-repository-squeeze_0.0.2_all.deb && dpkg -i globus-repository-squeeze_0.0.2_all.deb && aptitude update && tasksel install globus-gridftp && aptitude -y upgrade && aptitude -y install globus-simple-ca'
+  ssh p$a ln -s /home/protobench/ /var/www
+  ssh p$a invoke-rc.d nginx start
+  a=$(($a+1))
 done
 
 # FIXME upload the correct ips for pen
-ssh pp apt-get -y install haproxy pen
-ssh pp pen 21 -l pen.log -p pen.pid am0:21 am1:21 am2:21 am3:21 am4:21
+ssh pp apt-get -y install haproxy ftp-proxy
+
+scp haproxy.cfg.tmp pp:/etc/haproxy/haproxy.cfg
+rm haproxy.cfg.tmp
+#ssh pp pen 21 -l pen.log -p pen.pid am0:21 am1:21 am2:21 am3:21 am4:21
 
 for HOST in $CLIENTS; do
   ssh $HOST sudo apt-get install wget bittorrent build-essential
--- 52-http-matrix.sh
+++ 52-http-matrix.sh
@@ -6,13 +6,13 @@
 . clients.sh
 
 N=1 # must match haproxy's settings - number of servers. TODO this is not automated yet, unfortunately :(
-M=4 # number of clients to run. No feedback system, so TODO this is not automated yet as well :(
+M=1 # number of clients to run. No feedback system, so TODO this is not automated yet as well :(
 
 echo Run started
 for i in `seq $N`; do
   for m in `seq $M`; do
     HOST=$(echo $CLIENTS | awk "{ print \$$m }")
-    echo $HOST
+    echo $HOST "nohup /home/protobench/520-http-matrix-element.sh $N $M $i $PROXY </dev/null >/dev/null 2>&1 &"
     ssh $HOST "nohup /home/protobench/520-http-matrix-element.sh $N $M $i $PROXY </dev/null >/dev/null 2>&1 &"
   done
 done
